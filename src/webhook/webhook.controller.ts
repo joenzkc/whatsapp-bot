f3acb196a813eaa6feabea6130efea79c88c6071
joenzkc <77266967+joenzkc@users.noreply.github.com>
2022-07-15 13:24:27 +0800

    update

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -37,14 +37,39 @@ export class WebhookController {
 
   @Post('/webhook')
   async handleEvents(@Body() body: any) {
-    console.log(body.entry[0].changes[0]);
-    const sender = body.entry[0].changes[0].value.contacts[0];
-    const sender_id = sender.wa_id;
-    const sender_name = sender.profile.name;
-    const reply_message = `Hi ${sender_name}, your phone number is ${sender_id}!`;
-    console.log(reply_message);
-    const message = body.entry[0].changes[0].value.messages[0];
-    const received_text = message.text.body;
-    console.log(`Received: ${received_text}`);
+    if (body) {
+      if (
+        body.entry &&
+        body.entry[0].changes &&
+        body.entry[0].changes[0] &&
+        body.entry[0].changes[0].value.messages &&
+        body.entry[0].changes[0].value.messages[0]
+      ) {
+        const phone_number_id =
+          body.entry[0].changes[0].value.metadata.phone_number_id;
+        const sender = body.entry[0].changes[0].value.contacts[0];
+        const sender_id = sender.wa_id;
+        const sender_name = sender.profile.name;
+        const reply_message = `Hi ${sender_name}, your phone number is ${sender_id}!`;
+        console.log(reply_message);
+        const message = body.entry[0].changes[0].value.messages[0];
+        const from = message.from;
+        const received_text = message.text.body;
+        console.log(`Received: ${received_text}`);
+
+        axios.post(
+          `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+          {
+            messaging_product: 'whatsapp',
+            to: from,
+            text: { body: `${reply_message}` },
+          },
+        );
+
+        return 'Sent message';
+      } else {
+        throw new BadRequestException('Unknown command!');
+      }
+    }
   }
 }
