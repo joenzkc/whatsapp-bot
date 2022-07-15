aa87b08efa6cf4860682ce4547f320c662a11b04
joenzkc <77266967+joenzkc@users.noreply.github.com>
2022-07-15 10:34:33 +0800

    init

---
+++ src/webhook/webhook.controller.ts
@@ -0,0 +1,42 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
+const axios = require('axios');
+
+@Controller('webhook')
+export class WebhookController {
+  /**
+   * Facebook will call this API to verify the webhook
+   *
+   * Learn more at https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+   * @param mode
+   * @param challenge
+   * @param token
+   * @returns
+   */
+  @Get('/webhook')
+  async verify(
+    @Query('hub.mode') mode: String,
+    @Query('hub.challenge') challenge: String,
+    @Query('hub.verify_token') token: String,
+  ) {
+    if (mode && token) {
+      if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        console.log('WEBHOOK VERIFIED');
+        return challenge;
+      } else {
+        throw new BadRequestException('VERIFICATION FAILED');
+      }
+    }
+  }
+
+  @Post('/webhook')
+  async handleEvents(@Body() body: any) {
+    console.log(body);
+  }
+}
