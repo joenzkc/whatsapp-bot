135f24f9b14add8240e0787b51bc23c6d7be0461
joenzkc <77266967+joenzkc@users.noreply.github.com>
2022-07-18 12:01:18 +0800

    refactor

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -6,10 +6,12 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { WebhookService } from './webhook.service';
 const axios = require('axios');
 
 @Controller('webhook')
 export class WebhookController {
+  constructor(private readonly webhookService: WebhookService) {}
   /**
    * Facebook will call this API to verify the webhook
    *
@@ -37,214 +39,216 @@ export class WebhookController {
 
   @Post('/webhook')
   async handleEvents(@Body() body: any) {
-    if (body) {
-      console.log(body.entry[0].changes[0].value);
-      console.log(body.entry[0].changes[0].value.metadata);
+    return this.webhookService.handleEvents(body);
+    // if (body) {
+    //   console.log(body.entry[0].changes[0].value);
+    //   console.log(body.entry[0].changes[0].value.metadata);
 
-      if (
-        body.entry &&
-        body.entry[0].changes &&
-        body.entry[0].changes[0] &&
-        body.entry[0].changes[0].value.messages &&
-        body.entry[0].changes[0].value.messages[0]
-      ) {
-        const message = body.entry[0].changes[0].value.messages[0];
-        if (message.type && message.type === 'button') {
-          console.log(message.button);
-        }
+    //   if (
+    //     this.webhookService.isMessage(body)
+    //     // body.entry &&
+    //     // body.entry[0].changes &&
+    //     // body.entry[0].changes[0] &&
+    //     // body.entry[0].changes[0].value.messages &&
+    //     // body.entry[0].changes[0].value.messages[0]
+    //   ) {
+    //     const message = body.entry[0].changes[0].value.messages[0];
+    //     if (message.type && message.type === 'button') {
+    //       console.log(message.button);
+    //     }
 
-        if (message.type && message.type === 'interactive') {
-          console.log(message.interactive);
-        }
-        const phone_number_id =
-          body.entry[0].changes[0].value.metadata.phone_number_id;
-        const sender = body.entry[0].changes[0].value.contacts[0];
-        const sender_id = sender.wa_id;
-        const sender_name = sender.profile.name;
-        const reply_message = `Hi ${sender_name}, your phone number is ${sender_id}!`;
-        // console.log(reply_message);
-        const from = message.from;
-        const received_text = message.text.body;
-        // console.log(`Received: ${received_text}`);
-        const full_message =
-          reply_message + 'The message you sent me was: ' + received_text;
-        console.log(received_text);
-        if (received_text === 'action') {
-          const body = {
-            messaging_product: 'whatsapp',
-            to: from,
-            type: 'template',
-            template: {
-              name: 'message_test_two',
-              language: {
-                code: 'en_GB',
-              },
-            },
-          };
-          try {
-            await axios.post(
-              `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
-              body,
-            );
-            return;
-          } catch (err) {
-            console.log(err);
-            throw err;
-          }
-        }
+    //     if (message.type && message.type === 'interactive') {
+    //       console.log(message.interactive);
+    //     }
+    //     const phone_number_id =
+    //       body.entry[0].changes[0].value.metadata.phone_number_id;
+    //     const sender = body.entry[0].changes[0].value.contacts[0];
+    //     const sender_id = sender.wa_id;
+    //     const sender_name = sender.profile.name;
+    //     const reply_message = `Hi ${sender_name}, your phone number is ${sender_id}!`;
+    //     // console.log(reply_message);
+    //     const from = message.from;
+    //     const received_text = message.text.body;
+    //     // console.log(`Received: ${received_text}`);
+    //     const full_message =
+    //       reply_message + 'The message you sent me was: ' + received_text;
+    //     console.log(received_text);
+    //     if (received_text === 'action') {
+    //       const body = {
+    //         messaging_product: 'whatsapp',
+    //         to: from,
+    //         type: 'template',
+    //         template: {
+    //           name: 'message_test_two',
+    //           language: {
+    //             code: 'en_GB',
+    //           },
+    //         },
+    //       };
+    //       try {
+    //         await axios.post(
+    //           `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+    //           body,
+    //         );
+    //         return;
+    //       } catch (err) {
+    //         console.log(err);
+    //         throw err;
+    //       }
+    //     }
 
-        if (received_text === 'button') {
-          try {
-            const interactive = {
-              type: 'button',
-              header: {
-                type: 'text',
-                text: 'Welcome',
-              },
-              body: {
-                text: 'Welcome to my Chatbot. Select an option you would like',
-              },
-              action: {
-                buttons: [
-                  {
-                    type: 'reply',
-                    reply: {
-                      id: 'button-login',
-                      title: 'Login',
-                    },
-                  },
-                  {
-                    type: 'reply',
-                    reply: {
-                      id: 'button-forgot-password',
-                      title: 'Forgot Password',
-                    },
-                  },
-                  {
-                    type: 'reply',
-                    reply: {
-                      id: 'button-help',
-                      title: 'Help',
-                    },
-                  },
-                ],
-              },
-            };
+    //     if (received_text === 'button') {
+    //       try {
+    //         const interactive = {
+    //           type: 'button',
+    //           header: {
+    //             type: 'text',
+    //             text: 'Welcome',
+    //           },
+    //           body: {
+    //             text: 'Welcome to my Chatbot. Select an option you would like',
+    //           },
+    //           action: {
+    //             buttons: [
+    //               {
+    //                 type: 'reply',
+    //                 reply: {
+    //                   id: 'button-login',
+    //                   title: 'Login',
+    //                 },
+    //               },
+    //               {
+    //                 type: 'reply',
+    //                 reply: {
+    //                   id: 'button-forgot-password',
+    //                   title: 'Forgot Password',
+    //                 },
+    //               },
+    //               {
+    //                 type: 'reply',
+    //                 reply: {
+    //                   id: 'button-help',
+    //                   title: 'Help',
+    //                 },
+    //               },
+    //             ],
+    //           },
+    //         };
 
-            await axios.post(
-              `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
-              {
-                messaging_product: 'whatsapp',
-                to: from,
-                type: 'interactive',
-                interactive: interactive,
-              },
-            );
-            return;
-          } catch (err) {
-            throw err;
-          }
-        }
+    //         await axios.post(
+    //           `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+    //           {
+    //             messaging_product: 'whatsapp',
+    //             to: from,
+    //             type: 'interactive',
+    //             interactive: interactive,
+    //           },
+    //         );
+    //         return;
+    //       } catch (err) {
+    //         throw err;
+    //       }
+    //     }
 
-        if (received_text === 'start') {
-          try {
-            await axios.post(
-              `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
-              {
-                messaging_product: 'whatsapp',
-                to: sender_id,
-                type: 'template',
-                template: {
-                  name: 'testing_templates',
-                  language: {
-                    code: 'en_GB',
-                  },
-                  components: [
-                    {
-                      type: 'body',
-                      parameters: [
-                        {
-                          type: 'text',
-                          text: sender_name,
-                        },
-                        {
-                          type: 'text',
-                          text: sender_id,
-                        },
-                      ],
-                    },
-                  ],
-                },
-              },
-            );
-            return;
-          } catch (err) {
-            console.log(err);
-            throw err;
-          }
-        } else {
-          try {
-            const interactive = {
-              type: 'list',
-              header: {
-                type: 'text',
-                text: 'Welcome',
-              },
-              body: {
-                text: 'Welcome to my Chatbot. Select an option you would like',
-              },
-              action: {
-                button: 'View Options',
-                sections: [
-                  {
-                    title: 'Account',
-                    rows: [
-                      {
-                        id: 'account-login',
-                        title: 'Login',
-                        description: 'Log into your account',
-                      },
-                      {
-                        id: 'account-forgot',
-                        title: 'Forgot your password',
-                      },
-                    ],
-                  },
-                  {
-                    title: 'Cart',
-                    rows: [
-                      {
-                        id: 'cart-view',
-                        title: 'View my cart',
-                      },
-                      {
-                        id: 'cart-checkout',
-                        title: 'Checkout',
-                      },
-                    ],
-                  },
-                ],
-              },
-            };
+    //     if (received_text === 'start') {
+    //       try {
+    //         await axios.post(
+    //           `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+    //           {
+    //             messaging_product: 'whatsapp',
+    //             to: sender_id,
+    //             type: 'template',
+    //             template: {
+    //               name: 'testing_templates',
+    //               language: {
+    //                 code: 'en_GB',
+    //               },
+    //               components: [
+    //                 {
+    //                   type: 'body',
+    //                   parameters: [
+    //                     {
+    //                       type: 'text',
+    //                       text: sender_name,
+    //                     },
+    //                     {
+    //                       type: 'text',
+    //                       text: sender_id,
+    //                     },
+    //                   ],
+    //                 },
+    //               ],
+    //             },
+    //           },
+    //         );
+    //         return;
+    //       } catch (err) {
+    //         console.log(err);
+    //         throw err;
+    //       }
+    //     } else {
+    //       try {
+    //         const interactive = {
+    //           type: 'list',
+    //           header: {
+    //             type: 'text',
+    //             text: 'Welcome',
+    //           },
+    //           body: {
+    //             text: 'Welcome to my Chatbot. Select an option you would like',
+    //           },
+    //           action: {
+    //             button: 'View Options',
+    //             sections: [
+    //               {
+    //                 title: 'Account',
+    //                 rows: [
+    //                   {
+    //                     id: 'account-login',
+    //                     title: 'Login',
+    //                     description: 'Log into your account',
+    //                   },
+    //                   {
+    //                     id: 'account-forgot',
+    //                     title: 'Forgot your password',
+    //                   },
+    //                 ],
+    //               },
+    //               {
+    //                 title: 'Cart',
+    //                 rows: [
+    //                   {
+    //                     id: 'cart-view',
+    //                     title: 'View my cart',
+    //                   },
+    //                   {
+    //                     id: 'cart-checkout',
+    //                     title: 'Checkout',
+    //                   },
+    //                 ],
+    //               },
+    //             ],
+    //           },
+    //         };
 
-            await axios.post(
-              `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
-              {
-                messaging_product: 'whatsapp',
-                to: from,
-                type: 'interactive',
-                interactive: interactive,
-              },
-            );
-          } catch (err) {
-            throw err;
-          }
-        }
-        return 'Sent message';
-      } else {
-        console.log('Bad command');
-        throw new BadRequestException('Unknown command!');
-      }
-    }
+    //         await axios.post(
+    //           `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+    //           {
+    //             messaging_product: 'whatsapp',
+    //             to: from,
+    //             type: 'interactive',
+    //             interactive: interactive,
+    //           },
+    //         );
+    //       } catch (err) {
+    //         throw err;
+    //       }
+    //     }
+    //     return 'Sent message';
+    //   } else {
+    //     console.log('Bad command');
+    //     throw new BadRequestException('Unknown command!');
+    //   }
+    // }
   }
 }
--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,4 +1,444 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+const axios = require('axios');
 
 @Injectable()
-export class WebhookService {}
+export class WebhookService {
+  /**
+   * Handles the event
+   */
+  async handleEvents(body: any) {
+    if (body) {
+      console.log(body.entry[0].changes[0].value);
+      console.log(body.entry[0].changes[0].value.metadata);
+
+      if (this.isMessage(body)) {
+        const message = this.parseMessage(body);
+        if (this.isInteractiveResponse(message)) {
+          console.log(message.interactive);
+        }
+
+        if (this.isButtonResponse(message)) {
+          console.log(message.button);
+        }
+
+        const { phone_number_id, sender, sender_name, sender_number } =
+          this.parseValue(body);
+
+        const from = message.from;
+        const received_text = message.text.body;
+
+        switch (received_text) {
+          case 'action':
+            this.handleAction(from, phone_number_id);
+            break;
+          case 'button':
+            this.handleButton(from, phone_number_id);
+            break;
+          case 'start':
+            this.handleStart(phone_number_id, sender_number, sender_name);
+            break;
+          default:
+            this.handleDefault(phone_number_id, from);
+        }
+
+        return;
+
+        //   if (received_text === 'action') {
+        //     this.handleAction(from, phone_number_id)
+        //   }
+
+        //   if (received_text === 'button') {
+        //     try {
+        //       const interactive = {
+        //         type: 'button',
+        //         header: {
+        //           type: 'text',
+        //           text: 'Welcome',
+        //         },
+        //         body: {
+        //           text: 'Welcome to my Chatbot. Select an option you would like',
+        //         },
+        //         action: {
+        //           buttons: [
+        //             {
+        //               type: 'reply',
+        //               reply: {
+        //                 id: 'button-login',
+        //                 title: 'Login',
+        //               },
+        //             },
+        //             {
+        //               type: 'reply',
+        //               reply: {
+        //                 id: 'button-forgot-password',
+        //                 title: 'Forgot Password',
+        //               },
+        //             },
+        //             {
+        //               type: 'reply',
+        //               reply: {
+        //                 id: 'button-help',
+        //                 title: 'Help',
+        //               },
+        //             },
+        //           ],
+        //         },
+        //       };
+
+        //       await axios.post(
+        //         `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        //         {
+        //           messaging_product: 'whatsapp',
+        //           to: from,
+        //           type: 'interactive',
+        //           interactive: interactive,
+        //         },
+        //       );
+        //       return;
+        //     } catch (err) {
+        //       throw err;
+        //     }
+        //   }
+
+        //   if (received_text === 'start') {
+        //     try {
+        //       await axios.post(
+        //         `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        //         {
+        //           messaging_product: 'whatsapp',
+        //           to: sender_id,
+        //           type: 'template',
+        //           template: {
+        //             name: 'testing_templates',
+        //             language: {
+        //               code: 'en_GB',
+        //             },
+        //             components: [
+        //               {
+        //                 type: 'body',
+        //                 parameters: [
+        //                   {
+        //                     type: 'text',
+        //                     text: sender_name,
+        //                   },
+        //                   {
+        //                     type: 'text',
+        //                     text: sender_id,
+        //                   },
+        //                 ],
+        //               },
+        //             ],
+        //           },
+        //         },
+        //       );
+        //       return;
+        //     } catch (err) {
+        //       console.log(err);
+        //       throw err;
+        //     }
+        //   } else {
+        //     try {
+        //       const interactive = {
+        //         type: 'list',
+        //         header: {
+        //           type: 'text',
+        //           text: 'Welcome',
+        //         },
+        //         body: {
+        //           text: 'Welcome to my Chatbot. Select an option you would like',
+        //         },
+        //         action: {
+        //           button: 'View Options',
+        //           sections: [
+        //             {
+        //               title: 'Account',
+        //               rows: [
+        //                 {
+        //                   id: 'account-login',
+        //                   title: 'Login',
+        //                   description: 'Log into your account',
+        //                 },
+        //                 {
+        //                   id: 'account-forgot',
+        //                   title: 'Forgot your password',
+        //                 },
+        //               ],
+        //             },
+        //             {
+        //               title: 'Cart',
+        //               rows: [
+        //                 {
+        //                   id: 'cart-view',
+        //                   title: 'View my cart',
+        //                 },
+        //                 {
+        //                   id: 'cart-checkout',
+        //                   title: 'Checkout',
+        //                 },
+        //               ],
+        //             },
+        //           ],
+        //         },
+        //       };
+
+        //       await axios.post(
+        //         `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        //         {
+        //           messaging_product: 'whatsapp',
+        //           to: from,
+        //           type: 'interactive',
+        //           interactive: interactive,
+        //         },
+        //       );
+        //     } catch (err) {
+        //       throw err;
+        //     }
+        //   }
+        //   return 'Sent message';
+        // } else {
+        //   console.log('Bad command');
+        //   throw new BadRequestException('Unknown command!');
+      }
+    }
+  }
+
+  /**
+   * Check if the event is a message event
+   */
+  isMessage(body: any) {
+    return (
+      body.entry &&
+      body.entry[0].changes &&
+      body.entry[0].changes[0] &&
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0]
+    );
+  }
+
+  /**
+   * Check if the message is an interactive response
+   * @param message
+   * @returns
+   */
+  isInteractiveResponse(message: any) {
+    if (message.type) {
+      return message.type === 'interactive';
+    }
+    return false;
+  }
+
+  /**
+   * Check if the message is a button response
+   */
+  isButtonResponse(message: any) {
+    if (message.type) {
+      return message.type === 'button';
+    }
+    return false;
+  }
+
+  /**
+   * Parse the message event
+   */
+  parseMessage(body: any) {
+    return body.entry[0].changes[0].value.messages[0];
+  }
+
+  /**
+   * Parse values to get relevant information
+   */
+  parseValue(body: any) {
+    const phone_number_id =
+      body.entry[0].changes[0].value.metadata.phone_number_id;
+    const sender = body.entry[0].changes[0].value.contacts[0].profile;
+    const sender_name = sender.name;
+    const sender_number = sender.wa_id;
+    return { phone_number_id, sender, sender_name, sender_number };
+  }
+
+  /**
+   * Handler when the user types in action
+   */
+  async handleAction(from, phone_number_id) {
+    const body = {
+      messaging_product: 'whatsapp',
+      to: from,
+      type: 'template',
+      template: {
+        name: 'message_test_two',
+        language: {
+          code: 'en_GB',
+        },
+      },
+    };
+    try {
+      await axios.post(
+        `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        body,
+      );
+      return;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  /**
+   * Handler for when the user types button
+   */
+  async handleButton(from, phone_number_id) {
+    try {
+      const interactive = {
+        type: 'button',
+        header: {
+          type: 'text',
+          text: 'Welcome',
+        },
+        body: {
+          text: 'Welcome to my Chatbot. Select an option you would like',
+        },
+        action: {
+          buttons: [
+            {
+              type: 'reply',
+              reply: {
+                id: 'button-login',
+                title: 'Login',
+              },
+            },
+            {
+              type: 'reply',
+              reply: {
+                id: 'button-forgot-password',
+                title: 'Forgot Password',
+              },
+            },
+            {
+              type: 'reply',
+              reply: {
+                id: 'button-help',
+                title: 'Help',
+              },
+            },
+          ],
+        },
+      };
+
+      await axios.post(
+        `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        {
+          messaging_product: 'whatsapp',
+          to: from,
+          type: 'interactive',
+          interactive: interactive,
+        },
+      );
+      return;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  /**
+   * Handler for when the user types action
+   */
+  async handleStart(phone_number_id, sender_number, sender_name) {
+    try {
+      await axios.post(
+        `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        {
+          messaging_product: 'whatsapp',
+          to: sender_number,
+          type: 'template',
+          template: {
+            name: 'testing_templates',
+            language: {
+              code: 'en_GB',
+            },
+            components: [
+              {
+                type: 'body',
+                parameters: [
+                  {
+                    type: 'text',
+                    text: sender_name,
+                  },
+                  {
+                    type: 'text',
+                    text: sender_number,
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      );
+      return;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  /**
+   * Default case
+   */
+  async handleDefault(phone_number_id, from) {
+    try {
+      const interactive = {
+        type: 'list',
+        header: {
+          type: 'text',
+          text: 'Welcome',
+        },
+        body: {
+          text: 'Welcome to my Chatbot. Select an option you would like',
+        },
+        action: {
+          button: 'View Options',
+          sections: [
+            {
+              title: 'Account',
+              rows: [
+                {
+                  id: 'account-login',
+                  title: 'Login',
+                  description: 'Log into your account',
+                },
+                {
+                  id: 'account-forgot',
+                  title: 'Forgot your password',
+                },
+              ],
+            },
+            {
+              title: 'Cart',
+              rows: [
+                {
+                  id: 'cart-view',
+                  title: 'View my cart',
+                },
+                {
+                  id: 'cart-checkout',
+                  title: 'Checkout',
+                },
+              ],
+            },
+          ],
+        },
+      };
+
+      await axios.post(
+        `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.TOKEN}`,
+        {
+          messaging_product: 'whatsapp',
+          to: from,
+          type: 'interactive',
+          interactive: interactive,
+        },
+      );
+    } catch (err) {
+      throw err;
+    }
+  }
+}
